8f07900234380a3188300edf30c3505587eaf79e
shjeng <ghsmsl20@gmail.com>
2024-03-11 23:53:34 +0900

    스프링 데이터 JPA 리포지토리로 변경 사용자 정의 리포지토리 (페이징 연동 작성 예정)

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,7 +1,6 @@
 package study.querydsl.repository;
 
 import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -9,15 +8,13 @@ import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
-import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
-import study.querydsl.entity.QTeam;
 
 import java.util.List;
 import java.util.Optional;
 
-import static study.querydsl.entity.QMember.*;
+import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
 @Repository
@@ -29,48 +26,53 @@ public class MemberJpaRepository {
         this.em = em;
         this.queryFactory = new JPAQueryFactory(em);
     }
-    public void save(Member member){
+
+    public void save(Member member) {
         em.persist(member);
     }
 
-    public Optional<Member> findById(Long id){
+    public Optional<Member> findById(Long id) {
         Member member = em.find(Member.class, id);
         return Optional.ofNullable(member);
     }
 
-    public List<Member> findAll(){
+    public List<Member> findAll() {
         return em.createQuery("select m from Member m ", Member.class)
                 .getResultList();
     }
-    public List<Member> findAll_Querydsl(){
+
+    public List<Member> findAll_Querydsl() {
         return queryFactory
                 .selectFrom(member)
                 .fetch();
     }
-    public List<Member> findByUsername(String username){
+
+    public List<Member> findByUsername(String username) {
         return em.createQuery("select m from Member m where m.username =:username", Member.class)
-                .setParameter("username",username)
+                .setParameter("username", username)
                 .getResultList();
     }
-    public List<Member> findByUsername_Querydsl(String username){
+
+    public List<Member> findByUsername_Querydsl(String username) {
         return queryFactory
                 .selectFrom(member)
                 .where(member.username.eq(username))
                 .fetch();
     }
+
     // 동적 쿼리와 성능 최적화 조회 - Builder 사용
-    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition){
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
         BooleanBuilder builder = new BooleanBuilder();
-        if(StringUtils.hasText(condition.getUsername())){ // null 또는 ""로 들어올 수도 있으니 hasText 사용
+        if (StringUtils.hasText(condition.getUsername())) { // null 또는 ""로 들어올 수도 있으니 hasText 사용
             builder.and(member.username.eq(condition.getUsername()));
         }
-        if(StringUtils.hasText(condition.getTeamName())){
+        if (StringUtils.hasText(condition.getTeamName())) {
             builder.and(team.name.eq(condition.getTeamName()));
         }
-        if(condition.getAgeGoe() != null){
+        if (condition.getAgeGoe() != null) {
             builder.and(member.age.goe(condition.getAgeGoe()));
         }
-        if(condition.getAgeLoe() != null){
+        if (condition.getAgeLoe() != null) {
             builder.and(member.age.goe(condition.getAgeLoe()));
         }
         return queryFactory
@@ -81,13 +83,13 @@ public class MemberJpaRepository {
                         team.id.as("teamId"),
                         team.name.as("teamName")))
                 .from(member)
-                .leftJoin(member.team,team)
+                .leftJoin(member.team, team)
                 .where(builder)
                 .fetch();
     }
 
     // 동적 쿼리와 성능 최적화 조회 - Where절 파라미터 사용
-    public List<MemberTeamDto> search(MemberSearchCondition condition){
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
         return queryFactory
                 .select(new QMemberTeamDto(
                         member.id.as("memberId"),
@@ -96,7 +98,7 @@ public class MemberJpaRepository {
                         team.id.as("teamId"),
                         team.name.as("teamName")))
                 .from(member)
-                .leftJoin(member.team,team)
+                .leftJoin(member.team, team)
                 .where(
                         usernameEq(condition.getUsername()),
                         teamNameEq(condition.getTeamName()),
@@ -108,7 +110,7 @@ public class MemberJpaRepository {
     }
 
     private BooleanExpression usernameEq(String username) {
-        return StringUtils.hasText(username) ? member.username.eq(username):  null ;
+        return StringUtils.hasText(username) ? member.username.eq(username) : null;
     }
 
     private BooleanExpression teamNameEq(String teamName) {
@@ -116,12 +118,14 @@ public class MemberJpaRepository {
     }
 
     private BooleanExpression ageGoe(Integer ageGoe) {
-        return ageGoe != null ? member.age.goe(ageGoe) : null ;
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
     }
+
     private BooleanExpression ageLoe(Integer ageLoe) {
         return ageLoe != null ? member.age.loe(ageLoe) : null;
     }
-    private BooleanExpression ageBetween(int ageLoe, int ageGoe){
+
+    private BooleanExpression ageBetween(int ageLoe, int ageGoe) {
         return ageLoe(ageGoe).and(ageGoe(ageGoe));
     }
 }
